7346e80a397afde0826da82fd60db09e278ca770
GaoXiang <hughgao01@gmail.com>
2017-04-21 15:05:11 +0800

    add app test files

---
+++ src/App.tsx
@@ -0,0 +1,75 @@
+import * as React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import User from './components/user';
+import IForm from './components/form';
+import {formGroupProps} from './data';
+import IFormModal from './components/formModal';
+import Row from 'antd/lib/grid/row';
+import Col from 'antd/lib/grid/col';
+import {UploadModal} from './components/upload';
+import {Layout, message} from 'antd';
+import {default as Header, TitleLink} from './components/header';
+
+class App extends React.Component<{}, {}> {
+    render() {
+        const links: TitleLink[] = [
+            {name: 'Login', link: '/user/Login', iconType: 'setting'},
+            {link: '/user/ChangePass', name: 'Change Password'},
+            {link: '/Form', name: 'Form'},
+            {link: '/FormModal', name: 'FormModal'},
+            {link: '/Upload', name: 'Upload'},
+            {link: '/message', name: 'TestHeaderOnClick', onClick: () => message.success('successufl')}
+        ];
+        return (
+            <Layout className="App">
+                <Router>
+                    <div>
+                        <Header logoUrl="" titles={links}/>
+                        <Route path="/user/Login" component={UserLogin}/>
+                        <Route path="/user/ChangePass" component={UserChangePass}/>
+                        <Route path="/Form" component={IFormTest}/>
+                        <Route path="/FormModal" component={IFormModalTest}/>
+                        <Route path="/Upload" component={UploadTest}/>
+                    </div>
+                </Router>
+            </Layout>
+        );
+    }
+}
+
+class UserLogin extends React.Component<any, any> {
+    render() {
+        return ( <User operate="login" url="/user/login"/> );
+    }
+}
+class UserChangePass extends React.Component<any, any> {
+    render() {
+        return (<User operate="changePass" url="/user/changePass"/>);
+    }
+}
+
+class IFormTest extends React.Component<any, any> {
+    render() {
+        return (
+            <Row type="flex" justify="center">
+                <Col span={6}>
+                    <IForm url="/form/submit" formOptions={formGroupProps}/>
+                </Col>
+            </Row>
+        );
+    }
+}
+
+class IFormModalTest extends React.Component<any, any> {
+    render() {
+        return ( <IFormModal show={true} title="Form Modal Test" url="/form/submit" formOptions={formGroupProps}/>);
+    }
+}
+
+class UploadTest extends React.Component<any, any> {
+    render() {
+        return ( <UploadModal show={true} url="/upload/submit" accept=".xlsx"/>);
+    }
+}
+
+export default App;
--- webpack.config.ts
+++ webpack.config.ts
@@ -13,77 +13,80 @@ const dev = !(env && env === 'production');
  */
 const libraryName = 'now-antd';
 const plugins = [
-  new CheckerPlugin(),
-  new TsConfigPathsPlugin(),
-  new HtmlWebpackPlugin({
-    inject: true,
-    title: libraryName,
-    filename: 'index.html',
-    template: join(__dirname, 'template/index.html'),
-    hash: true,
-    chunks: ['common', 'index']
-  })
+    new CheckerPlugin(),
+    new TsConfigPathsPlugin(),
+    new HtmlWebpackPlugin({
+        inject: true,
+        title: libraryName,
+        filename: 'index.html',
+        template: join(__dirname, 'template/index.html'),
+        hash: true,
+        chunks: ['common', 'index']
+    })
 ];
 
 let entry: string | string[] = [
-  // 'react-hot-loader/patch',
-  `webpack-dev-server/client?http://localhost:${serverPort}`,
-  // bundle the client for webpack-dev-servers and connect to the provided endpoint
-  'webpack/hot/only-dev-server',
-  // bundle the client for hot reloading
-  `./src/${libraryName}.ts`
+    // 'react-hot-loader/patch',
+    `webpack-dev-server/client?http://localhost:${serverPort}`,
+    // bundle the client for webpack-dev-servers and connect to the provided endpoint
+    'webpack/hot/only-dev-server',
+    // bundle the client for hot reloading, this one is for test app
+    `./src/index.tsx`
 ];
 
 if (dev === false) {
-  plugins.push(new TypedocWebpackPlugin(
-    {
-      theme: 'minimal',
-      out: 'docs',
-      target: 'es6',
-      ignoreCompilerErrors: true
-    },
-    'src'
-  ));
-  entry = join(__dirname, `src/${libraryName}.ts`);
+    plugins.push(new TypedocWebpackPlugin(
+        {
+            theme: 'minimal',
+            out: 'docs',
+            target: 'es6',
+            ignoreCompilerErrors: true
+        },
+        'src'
+    ));
+    // this is the real entry: now-antd.d.ts
+    entry = join(__dirname, `src/${libraryName}.ts`);
 } else {
-  plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new webpack.HotModuleReplacementPlugin());
 }
 
 export default {
-  entry: {
-    index: entry
-  },
-  // Currently cheap-module-source-map is broken https://github.com/webpack/webpack/issues/4176
-  devtool: 'source-map',
-  output: {
-    path: join(__dirname, 'dist'),
-    libraryTarget: 'umd',
-    library: camelCase(libraryName),
-    filename: `${libraryName}.js`
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        use: [
-          {
-            loader: 'awesome-typescript-loader'
-          }
+    entry: {
+        index: entry
+    },
+    // Currently cheap-module-source-map is broken https://github.com/webpack/webpack/issues/4176
+    devtool: 'source-map',
+    output: {
+        path: join(__dirname, 'dist'),
+        libraryTarget: 'umd',
+        library: camelCase(libraryName),
+        filename: `${libraryName}.js`
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                use: [
+                    {
+                        loader: 'babel-loader'
+                    }, {
+                        loader: 'awesome-typescript-loader'
+                    }
+                ]
+            }, {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader?importLoaders=1!postcss-loader'
+            },
         ]
-      }, {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?importLoaders=1!postcss-loader'
-      },
-    ]
-  },
-  plugins: plugins,
-  devServer: {
-    hot: true,
-    contentBase: resolve(__dirname, 'dist'),
-    port: serverPort,
-    publicPath: '/'
-  }
+    },
+    plugins: plugins,
+    devServer: {
+        hot: true,
+        contentBase: resolve(__dirname, 'dist'),
+        port: serverPort,
+        publicPath: '/'
+    }
 };
